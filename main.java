f7ecff59cb16c04d634f5fbba447213f8564ebef
Emre Canpolat <35192029+emrecanpolat@users.noreply.github.com>
2023-12-24 16:10:52 +0300

    Update main.java
    3 deneme hakkı eklendi

--- main.java
+++ main.java
@@ -9,15 +9,23 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String customerPassword = "12345";
         boolean passwordSuccessfull = false;
+        int denemeSayisi = 0;
 
-        while(!passwordSuccessfull) {
-
+        while(!passwordSuccessfull && denemeSayisi<3) {
+            denemeSayisi+=1;
             System.out.println("Hesap şifrenizi giriniz:");
             String typedPassword = scanner.next();
 
             if(customerPassword.contentEquals(typedPassword)) {
                 passwordSuccessfull = true;
                 System.out.println("Sisteme başarılı giriş yaptınız!");
+            }else {
+                if ((3-denemeSayisi)==0){
+                    System.out.println("3 kez üst üste hatalı şifre girdiniz. Hesabınız bloke oldu. Lütfen müşteri hizmetleri ile iletişime geçiniz.");
+                }else{
+                    System.out.println("Hatalı şifre girdiniz. Lütfen tekrar deneyin. Kalan deneme hakkı: " + (3-denemeSayisi));
+
+                }
             }
         }
 
